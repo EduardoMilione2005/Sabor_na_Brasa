38c6e7c491df6647980e1def11c85ff300959f56
Eduardo <eduardo.milione8@gmail.com>
2026-06-22 09:24:06 -0300

    feat(entrega): implementa SistemaEntregaLegado para despacho e rastreamento de entregas

---
+++ src/main/java/com/sabornabrasa/entrega/SistemaEntregaLegado.java
@@ -0,0 +1,8 @@
+package com.sabornabrasa.entrega;
+
+public class SistemaEntregaLegado {
+    public void despacharMercadoria(String endereco, String produto) {}
+    public String rastrearCodigo(String codigo) {
+        return "Rastreio " + codigo + ": em transito";
+    }
+}
